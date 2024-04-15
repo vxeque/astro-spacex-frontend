e5887f5e0c0a932bcca12cf0536c23b9016b2acf
vxeque <vxeque11@gmail.com>
2024-04-14 23:34:43 -0600

    spacex first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
 export default defineConfig({
-  site: 'https://vxeque.github.io/dist/client/',
+  site: 'https://vxeque.github.io/',
   base:"/",
   integrations: [tailwind()],
   output: 'hybrid',
+  build:{
+    client:'./src/pages'
+  },
   adapter: node({
     mode: "standalone"
   })
